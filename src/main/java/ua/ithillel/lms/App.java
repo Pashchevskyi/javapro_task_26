5bc586e3d5e2b08f62b20744b25167c2478d07cc
Pavlo Pashchevskyi <googalltooth4@gmail.com>
2023-02-22 20:52:55 +0200

    default controller has been registered as bean

--- src/main/java/ua/ithillel/lms/App.java
+++ src/main/java/ua/ithillel/lms/App.java
@@ -4,22 +4,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.event.EventListener;
 import ua.ithillel.lms.configuration.AppConfiguration;
+import ua.ithillel.lms.controller.DefaultController;
 
 /**
  * Hello world!
  */
 @Slf4j
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
   @Autowired
   private AppConfiguration myConfig;
 
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+    SpringApplication sa = new SpringApplication(App.class);
+    sa.run(args);
   }
 
   @EventListener(ApplicationReadyEvent.class)
@@ -28,5 +32,10 @@ public class App {
     log.info("name: {}", myConfig.getName());
     log.info("enabled:{}", myConfig.isEnabled());
     log.info("servers: {}", myConfig.getServers());
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(DefaultController.class);
   }
 }
